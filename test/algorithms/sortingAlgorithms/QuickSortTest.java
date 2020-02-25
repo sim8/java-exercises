133e300f7ad72d4ff7218f30664615e6e4998a97
sim8 <slees@hubspot.com>
2020-02-25 08:56:04 +0000

    Import classes to tests

--- test/algorithms/sortingAlgorithms/QuickSortTest.java
+++ test/algorithms/sortingAlgorithms/QuickSortTest.java
@@ -2,9 +2,13 @@ package algorithms.sortingAlgorithms;
 import org.junit.Assert;
 import org.junit.Test;
 
+import co.uk.simeonlees.algorithms.QuickSort;
+
 public class QuickSortTest {
   @Test
   public void firstTest() {
+    QuickSort quickSort = new QuickSort();
+//    quickSort.sort()
     Assert.assertTrue(true);
   }
 }
